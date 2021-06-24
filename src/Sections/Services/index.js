ce1a373afa005f387810d15cc653e6b099a724fe
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-06-24 16:31:21 +0900

    working on about section change layout

--- src/Sections/Services/index.js
+++ src/Sections/Services/index.js
@@ -1,7 +1,21 @@
 // This is the services section.  It details the possible services offered
+import styled from 'styled-components'
+
+const ServicesContainer = styled.div`
+  background-color: var(--black);
+  width: 100%;
+`
+
+const Title = styled.h1`
+  color: var(--white);
+`
 
 const ServicesSection = () => {
-  return <div>Service Section</div>
+  return (
+    <ServicesContainer>
+      <Title>Sections</Title>
+    </ServicesContainer>
+  )
 }
 
 export default ServicesSection
