f0cb7a1a4b655e5a56c1e1046c12f882dc34ea9e
nicholas de waal <nick.a.dewaal@gmail.com>
2021-06-28 16:28:28 +0900

    working on projects and services wire framing

--- src/Sections/Projects/index.js
+++ src/Sections/Projects/index.js
@@ -1,17 +1,83 @@
 import React from 'react'
 import styled from 'styled-components'
+import sitOnLedge from '../../assets/sitOnLedge.svg'
+import sitOnLedgeLong from '../../assets/sitOnLedgeLong.svg'
 
 const ProjectsContainer = styled.div`
-  min-height: 80vh;
-  background-color: var(--black);
+  min-height: calc(30rem + 50vw);
   width: 100vw;
+  /* min-height: 80vh; */
+  background-color: var(--black);
+
+  position: relative;
+  display: flex;
+  /* padding: calc(10rem + 15vw) 0 calc(2rem + 5vw) 0; */
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 
   color: white;
   /* margin-top: -1rem; */
 `
 
+const SitOnLedge = styled.div`
+  position: absolute;
+  width: 125vw;
+  height: auto;
+  left: -calc(10rem + 10vw);
+  top: 10rem;
+
+  /* max-width: 40rem; */
+  bottom: calc(2rem + 5vw);
+  /* transform: translateY(-50%); */
+`
+
+const Main = styled.div``
+
+const TitleContainer = styled.div`
+  position: absolute;
+  right: 20vw;
+  top: 10vw;
+
+  @media only Screen and (max-width: 48em) {
+    position: static;
+    /* left: 50%;
+    transform: translateX(-50%); */
+  }
+`
+
+const Title = styled.h1`
+  font-size: calc(3rem + 1vw);
+
+  @media only Screen and (max-width: 48em) {
+    font-size: calc(2rem + 2vw);
+  }
+`
+
+const CurvedLine = styled.div`
+  width: 9rem;
+  height: 2rem;
+  border: 5px solid var(--purple);
+  border-color: var(--purple) transparent transparent transparent;
+  border-radius: 200% / 80px 110px 0 0;
+`
+
 const ProjectSection = () => {
-  return <ProjectsContainer></ProjectsContainer>
+  return (
+    <ProjectsContainer>
+      <SitOnLedge>
+        <img src={sitOnLedgeLong} alt='man on ledge' />
+      </SitOnLedge>
+      <Main>
+        <TitleContainer>
+          <Title>Projects</Title>
+          <CurvedLine />
+        </TitleContainer>
+      </Main>
+    </ProjectsContainer>
+  )
 }
 
 export default ProjectSection
--- src/Sections/Services/index.js
+++ src/Sections/Services/index.js
@@ -2,17 +2,110 @@
 import styled from 'styled-components'
 import wave3 from '../../assets/wave3.svg'
 import wave2 from '../../assets/wave2.svg'
+import layingDown from '../../assets/layingDown.svg'
+import sphere from '../../assets/sphere.svg'
 
 const ServicesContainer = styled.div`
+  width: 100vw;
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  background-color: var(--black);
-  width: 100%;
+  overflow: hidden;
+  /* padding: 12.25px 0; */
+  min-height: 90vh;
+  background: rgb(239, 247, 249);
+  background: linear-gradient(
+    120deg,
+    rgba(239, 247, 249, 1) 0%,
+    rgba(210, 224, 228, 1) 100%
+  );
+
+  @media only Screen and (max-width: 48em) {
+    min-height: 70vh;
+  }
+`
+
+const LayingDown = styled.div`
+  position: absolute;
+  top: 5px;
+  right: 11vw;
+  width: 25%;
+  z-index: 10;
+
+  img {
+    transform: rotateY(180deg);
+  }
+
+  @media only Screen and (max-width: 48em) {
+    right: calc(7rem + 5vw);
+  }
+
+  @media only Screen and (max-width: 40em) {
+    right: calc(3rem + 5vw);
+  }
+`
+
+const Sphere = styled.div`
+  position: absolute;
+  width: 45vw;
+  max-width: calc(30rem + 5vw);
+  left: -5rem;
+  /* top: 40%; */
+  bottom: 0;
+  /* left: 70%; */
+  opacity: 70%;
+  
+
+  z-index: 10;
+
+  @media only Screen and (max-width: 48em) {
+    width: calc(50vw);
+  }
+
+  @media only Screen and (max-width: 40em) {
+    width: calc(60vw);
+  }
+`
+
+// const Tube = styled.div`
+//   position: absolute;
+//   width: 50vw;
+//   max-width: calc(28rem + 5vw);
+
+//   /* top: 40%; */
+//   left: -5rem;
+//   bottom: 0;
+//   /* left: 70%; */
+//   opacity: 50%;
+
+//   z-index: 10;
+
+//   /* @media only Screen and (max-width: 48em) {
+//     width: calc(50vw);
+//   }
+
+//   @media only Screen and (max-width: 40em) {
+//     width: calc(60vw);
+//   } */
+// `
+
+const TitleContainer = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+
+  @media only Screen and (max-width: 48em) {
+    position: static;
+    /* left: 50%;
+    transform: translateX(-50%); */
+  }
 `
 
 const WaveTop = styled.img`
   width: 100%;
+
+  transform: rotateY(180deg);
   /* position: absolute; */
   /* top: -1px; */
 `
@@ -27,6 +120,12 @@ const Main = styled.div``
 const ServicesSection = () => {
   return (
     <ServicesContainer>
+      <LayingDown>
+        <img src={layingDown} alt='sitting man' />
+      </LayingDown>
+      <Sphere>
+        <img src={sphere} alt='sphere' />
+      </Sphere>
       <WaveTop src={wave3} alt='wave' width='400'></WaveTop>
       <Main>Sections</Main>
       <WaveBottom src={wave2} alt='wave2' width='400'></WaveBottom>
