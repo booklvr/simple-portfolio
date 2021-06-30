a559f71a0c2046063752ac9f69ac7cae5a0d05de
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-06-30 16:28:45 +0900

    going home

--- src/Sections/Projects/index.js
+++ src/Sections/Projects/index.js
@@ -10,7 +10,7 @@ import '../../../node_modules/slick-carousel/slick/slick-theme.css'
 const ProjectsContainer = styled.div`
   width: 100vw;
 
-  min-height: calc(10rem + 70vw);
+  height: 55rem;
   background-color: var(--black);
   /* background-color: darkblue; */
 
@@ -31,11 +31,10 @@ const ProjectsContainer = styled.div`
 `
 
 const SitOnLedge = styled.div`
-  height: 40vh;
-
+  height: 40rem;
   top: 5rem;
-  right: 5rem;
-  display: block;
+  right: 3rem;
+  position: absolute;
 
   /* background: white; */
   /* min-height: calc(20rem + 60vw); */
@@ -44,7 +43,7 @@ const SitOnLedge = styled.div`
 
   img {
     position: absolute;
-    height: 60%;
+    height: 100%;
     width: auto;
     right: 5rem;
   }
@@ -56,22 +55,19 @@ const SitOnLedge = styled.div`
   /* bottom: calc(2rem + 5vw); */
   /* transform: translateY(-50%); */
 
-  @media only Screen and (max-width: 48em) {
-    width: 140vw;
-    right: -5rem;
-    /* top: 3rem; */
-  }
+  @media only Screen and (max-width: 80em) {
+    right: 0;
 
-  @media only Screen and (max-width: 40em) {
-    /* width: 70rem; */
-    /* right: -6rem; */
+    img {
+      right: -2rem;
+    }
   }
 `
 
 const TitleContainer = styled.div`
   position: absolute;
   right: 50%;
-  top: calc(4rem + 1vw);
+  top: 5rem;
   transform: translateX(50%);
 
   @media only Screen and (max-width: 48em) {
@@ -103,19 +99,32 @@ const CurvedLine = styled.div`
 
 const Carousel = styled.div`
   position: absolute;
-  top: 5rem;
+  top: 14.5rem;
   left: 5rem;
-  width: 65vw;
+  width: calc(100vw - 30rem);
+  height: 30.25rem;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
+  /* background: orange; */
+  /* padding-bottom: 5rem; */
 
-  .slick-slider .slick-arrow {
-    top: 75%;
+  @media only Screen and (max-width: 92em) {
+    /* background: pink; */
+    width: calc(100vw - 25rem);
+    left: 3rem;
+
+    .slick-slider .slick-arrow {
+      top: 100%;
+    }
+  }
+
+  @media only Screen and (max-width: 80em) {
+    width: calc(100vw - 17rem);
   }
 
   @media only Screen and (max-width: 40em) {
-    width: 90vw;
+    /* width: 90vw; */
 
     .slick-slider .slick-arrow {
       display: none;
@@ -125,14 +134,19 @@ const Carousel = styled.div`
     color: var(--white);
     font-size: calc(1.5rem + 1vw);
 
+    @media only Screen and (max-width: 92em) {
+      font-size: 3rem;
+    }
+
     @media only Screen and (max-width: 40em) {
       display: none;
     }
   }
 
-  .slick-slider .slick-dots .slick-slider .slick-dots button:before {
+  .slick-slider .slick-dots button:before {
     color: var(--white);
     font-size: calc(1.5rem + 0.25vw);
+    /* padding-bottom: 3rem; */
   }
 
   .slick-slide.slick-active {
@@ -142,7 +156,10 @@ const Carousel = styled.div`
     flex-direction: column;
     margin: 0;
     padding: 0;
-    margin-bottom: 3rem;
+    width: 100%;
+    /* padding-left: 1.1rem; */
+
+    /* margin-bottom: 3rem; */
   }
 `
 
@@ -168,23 +185,31 @@ const ProjectSection = () => {
         <Slider {...settings}>
           <Card
             title='Classpoint'
-            text='CodeBucks has been essential part of our business. I would definetly recommend CodeBucks. It has been amazing to have them.'
-            name='Jenny (CodeCall)'
+            text='Classpoint helps teachers to keep track of their students points for classroom games.  Classlists are saved, and games can be set up as individual games, team games, or boys vs girls.'
+            image='classpoint_thumbnail'
+            link='https://classpoint.ca'
+            git='https://github.com/booklvr/class_point2'
           />
           <Card
             title='Scategories'
-            text='CodeBucks has been essential part of our business. I would definetly recommend CodeBucks. It has been amazing to have them.'
-            name='Jenny (CodeCall)'
+            text='Scattergories is a creative-thinking category-based game.  The objective is to score points by uniquely naming objects within a set of categories, given an initial letter, within a time limit.'
+            image='scategories_thumbnail'
+            link='https://scategories.classpoint.ca'
+            git='https://github.com/booklvr/scategories-esl'
           />
           <Card
             title='SpeedQuiz'
-            text='CodeBucks has been essential part of our business. I would definetly recommend CodeBucks. It has been amazing to have them.'
-            name='Jenny (CodeCall)'
+            text='This site facilitates a charades type game, intended for English students.  Students are given a list of words and they must help their teammates guess as many words as possible within a time limit, without saying the word.'
+            image='speedquiz_thumbnail'
+            link='https://speedquiz.classpoint.ca'
+            git='https://github.com/booklvr/speedQuiz'
           />
           <Card
             title='ConnectFour'
-            text='CodeBucks has been essential part of our business. I would definetly recommend CodeBucks. It has been amazing to have them.'
-            name='Jenny (CodeCall)'
+            text='This site uses connect four, a tic-tac-toe-like game in which players alternately place pieces on a vertical board where the first player to achieve a line of four connected pieces wins the game.  It is adapted as a tool to teach English as students must call out some target language to choose their column.'
+            image='connectfour_thumbnail'
+            link='https://connectfour.classpoint.ca'
+            git='https://github.com/booklvr/connectFour'
           />
         </Slider>
       </Carousel>
