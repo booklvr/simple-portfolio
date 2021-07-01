a99a8cae6d4a827c830592699fd27f290c078a0e
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-07-02 07:30:55 +0900

    working on services wireframe

--- src/Sections/Services/index.js
+++ src/Sections/Services/index.js
@@ -104,7 +104,10 @@ const TitleContainer = styled.div`
 `
 
 const WaveTop = styled.img`
+  position: absolute;
   width: 100%;
+  top: 0;
+  z-index: 9;
 
   /* transform: rotateY(180deg); */
   /* position: absolute; */
@@ -112,14 +115,18 @@ const WaveTop = styled.img`
 `
 
 const WaveBottom = styled.img`
+  position: absolute;
   width: 100%;
+  bottom: 0;
   z-index: 9;
 `
 
 const ServiceSection = styled.div`
-  height: 30rem;
+  height: calc(2rem + 45vw);
+  height: 44.4vw;
   width: 100%;
   position: relative;
+  border: 1px solid orange;
 
   background: rgb(239, 247, 249);
   background: linear-gradient(
@@ -127,24 +134,54 @@ const ServiceSection = styled.div`
     rgba(239, 247, 249, 1) 0%,
     rgba(210, 224, 228, 1) 100%
   );
+  background: pink;
+`
+
+const TextContainerOne = styled.div`
+  position: absolute;
+  left: 22vw;
+  top: 50%;
+  height: 10vw;
+  width: 23vw;
+  transform: translateY(-50%);
+`
+
+const Text = styled.div`
+  width: 100%;
+  height: 100%;
+  font-size: calc(0.5rem + 1vw);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const ServicesSection = () => {
   return (
     <ServicesContainer>
-      <LayingDown>
-        <img src={layingDown} alt='sitting man' />
-      </LayingDown>
       <Sphere>
         <img src={sphere} alt='sphere' />
       </Sphere>
-      <WaveTop src={wave3} alt='wave' width='400'></WaveTop>
+
       <ServiceSection>
+        <WaveTop src={wave3} alt='wave' width='400'></WaveTop>
+        <LayingDown>
+          <img src={layingDown} alt='sitting man' />
+        </LayingDown>
+        <TextContainerOne>
+          <Text>
+            Working with the best designers to bring you creative and designs.
+          </Text>
+        </TextContainerOne>
+
         <WaveBottom src={wave2} alt='wave2' width='400'></WaveBottom>
-        <WaveTop src={wave4} alt='wave4' width='400'></WaveTop>
       </ServiceSection>
-      <ServiceSection></ServiceSection>
       <ServiceSection>
+        <WaveTop src={wave4} alt='wave' width='400'></WaveTop>
+        <WaveBottom src={wave2} alt='wave2' width='400'></WaveBottom>
+      </ServiceSection>
+      <ServiceSection>
+        <WaveTop src={wave3} alt='wave' width='400'></WaveTop>
         <WaveBottom src={wave2} alt='wave2' width='400'></WaveBottom>
       </ServiceSection>
     </ServicesContainer>
